eb12e3686cb608bae5c5ee9f9cf3859da023223b
manojtuguru <manojtuguru@gmail.com>
2017-03-19 15:31:42 -0700

    Changed README.md and test cases to Echo Assertions

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-Vicarious Coding Challenge:
-Code Logic
+                                                                       Vicarious Coding Challenge
 
-1.	Hosted a simple HTTP Server using python BaseHTTP server module.
-2.	Handler class – A simple class to handle the GET & POST requests for the calls made by the server 
-3.	Index.html – Loads when the service starts and input to be converted to Pig Latin
-4.	A simple logic to check if the starting letter are vowels or constants, add corresponding ‘yay’ or ‘ay’ and returns the elements
-5.	CGI(common gateway interface) library is being used here to parse the input from the user
 Running the application
 1.	Clone the GIT repository to your local through GIT clone or downloading as a Zip file
 2.	Change your current working directory to the downloaded file directory 
-3.	Since, port 80 is predefined we need to have sudo access to run the code
-4.	Run sudo python Vicarious.py 
+3.	Port 80 is predefined we need to have sudo access to run the Service
+4.	Run sudo python VicariousServer.py 
 5.	Open your Browser and type localhost to access the Microservice
 
 Running the Test Suite
 1.	The test suite is to check the range of different inputs against the microservice and check for the desired outputs
 2.	A shell script is written to test for the different inputs using curl 
 3.	Execute ./test.sh  on the command line
-4.	There will be a list of tests that passed or failed
+4.	There will be a list of tests that passed or failed with the corresponding input asserted towards the outputs
 
-
+Code Logic
+1.      Hosted a simple HTTP Server using python BaseHTTP server module.
+2.      Handler class – A class to handle the GET & POST requests for the calls made to the server
+3.      A simple POST call in the terminal when the service is running will convert the Input string to PigLatin
+4.      Index.html – To access on the browser, Point the browser to localhost:80 and input the word/paragraph to be converted into Pig Latin
+5.      The input is parsed to check if the starting letter are vowels or constants, append corresponding ‘yay’ or ‘ay’ with change in pattern and returns the PigLatin
+6.      CGI(common gateway interface) is the external library used to parse the input from the html 
--- test.sh
+++ test.sh
@@ -1,117 +1,119 @@
 #!/bin/sh 
+echo 'Vicarious'
+result=$(curl -s -XPOST --data "input=vicarious is really cOOl compAny and I Would Love To work With Them" localhost:80/vicarious)
+if [ "$result" == "icariousvay isyay eallyray OOlcay ompAnycay andyay Iyay ouldWay oveLay oTay orkway ithWay emThay " ]; then
+	echo "The expected result \"$result\" matched the actual result \"icariousvay isyay eallyray OOlcay ompAnycay andyay Iyay ouldWay oveLay oTay orkway ithWay emThay\" for the test case \"vicarious is really cOOl compAny and I Would Love To Work With Them\" passed"
+else
+	echo "The expected result \"$result\" did not match the actual result \"icariousvay isyay eallyray OOlcay ompAnycay andyay Iyay ouldWay oveLay oTay orkway ithWay emThay\" for the test case  \"vicarious is really cOOl compAny and I Would Love To Work With Them\" failed"
+fi
+
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=pig" localhost:80/vicarious)
 if [ "$result" == "igpay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actualy result \"igpay\" for the test case \"pig\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"igpay\" for test case \"pig\" failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=banana" localhost:80/vicarious)
 if [ "$result" == "ananabay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"ananabay\" for the test case \"banana\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"ananabay\" for  test case \"banana\" failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=trash" localhost:80/vicarious)
 if [ "$result" == "ashtray " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result  \"ashtray\" for the test case \"trash\"  passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result  \"$result\" did not match the actual result \"ashtray\" for test case \"trash\"  failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=duck" localhost:80/vicarious)
 if [ "$result" == "uckday " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result  \"uckday\" for the test case \"duck\"  passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result  \"uckday\" for the test case  \"duck\" failed"
 fi
 
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=glove" localhost:80/vicarious)
 if [ "$result" == "oveglay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result  \"oveglay\" for the test case \"glove\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result  \"$result\" did not match the actual result \"oveglay\" for the test case  \"glove\" failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=eat" localhost:80/vicarious)
 if [ "$result" == "eatyay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result  \"$result\" matched the actual result \"eatyay\" for the test case \"eat\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result  \"$result\" did not match the actual result  \"eatyay\"  for the test case  \"eat\" failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=omelet" localhost:80/vicarious)
 if [ "$result" == "omeletyay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"omeletyay\" for the test case \"omelet\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"omeletyay\"  for the test case  \"omelet\" failed"
 fi
 
 echo 'Testing the Standard Inputs'
 result=$(curl -s -XPOST --data "input=are" localhost:80/vicarious)
 if [ "$result" == "areyay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"areyay\" for the test case \"are\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result  \"$result\" did not match the actual result \"areyay\" for the test case  \"are\" failed"
 fi
 
 echo 'Testing the  Inputs in uppercase in the middle of the word'
 result=$(curl -s -XPOST --data "input=frUit" localhost:80/vicarious)
 if [ "$result" == "Uitfray " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"Uitfray\" for the test case \"frUit\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual  result \"Uitfray\" for the test case  \"frUit\" failed"
 fi
 
 echo 'Testing the input in  uppercase for a consonant'
 result=$(curl -s -XPOST --data "input=Fruit" localhost:80/vicarious)
 if [ "$result" == "uitFray " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"uitFray\" for the test case \"Fruit\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"uitFray\" for the test case  \"Fruit\" failed"
 fi
 
 
 echo 'Testing the  input in uppercase for a vowel'
 result=$(curl -s -XPOST --data "input=Apple" localhost:80/vicarious)
 if [ "$result" == "Appleyay " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"Appleyay\" for the test case \"Apple\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"Appleyay\" for the test case  \"Apple\" failed"
 fi
 
 echo 'Testing a sentence'
 result=$(curl -s -XPOST --data "input=The quick brown fox jump over the lazy dog" localhost:80/vicarious)
 if [ "$result" == "eThay uickqay ownbray oxfay umpjay overyay ethay azylay ogday " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"eThay uickqay ownbray oxfay umpjay overyay ethay azylay ogday \" for the test case \"The quick brown fox jump over the lazy dog\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"ueThay uickqay ownbray oxfay umpjay overyay ethay azylay ogday \" for the test case  \"The quick brown fox jump over the lazy dog\" failed"
 fi
 
 echo 'Testing a sentence'
 result=$(curl -s -XPOST --data "input=Hen, pig, fox, dOg" localhost:80/vicarious)
 if [ "$result" == "en,Hay ig,pay ox,fay Ogday " ]; then
-	echo "The expected result matched the actual result test passed"
+	echo "The expected result \"$result\" matched the actual result \"en,Hay ig,pay ox,fay Ogday \" for the test case \"Hen, pig, fox, dOg\" passed"
 else
-	echo "The expected result did not match the actual result test failed"
+	echo "The expected result \"$result\" did not match the actual result \"en,Hay ig,pay ox,fay Ogday \" for the test case  \"Hen, pig, fox, dOg\" failed"
 fi
 
-echo 'Testing a sentence'
-result=$(curl -s -XPOST --data "input=Hen. pig. fox. dOg" localhost:80/vicarious)
-if [ "$result" == "en.Hay ig.pay ox.fay Ogday " ]; then
-	echo "The expected result matched the actual result test passed"
-else
-	echo "The expected result did not match the actual result test failed"
-fi
 
-# echo 'Testing a sentence with Periods'
-# curl -XPOST --data "input=Hen.pig.fox.dOg" localhost:80/vicarious
+
+
+
