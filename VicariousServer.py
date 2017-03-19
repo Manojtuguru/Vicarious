06695c0869c94afd076b0ecc2354c5653ed05ae7
manojtuguru <manojtuguru@gmail.com>
2017-03-19 14:32:04 -0700

    First commit

---
+++ VicariousServer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import cgi
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+PORT_NUMBER = 8000 
+class myHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		f = open("index.html")
+		self.wfile.write(f.read())
+		f.close()
+		return
+	def do_POST(self):
+		if '/vicarious' in self.path:
+			form=cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD':'POST',
+				'CONTENT_TYPE':self.headers['Content-Type']})
+			words = str(form['input'])
+			words= words[27:]
+			words=words[:-2]
+			LatinWords = []
+			PigLatin = ''
+			wordlist = words.split()
+			for i in wordlist:
+				print i
+				if i[0] in 'aeiou' or i[0] in 'AEIOU':
+					print "inside vowel {}".format(i[0])
+					temp = ''.join([i,'yay'])
+					LatinWords.append(temp)
+				else:
+					vowel=0
+					for j in range(len(i)):
+						print i[j]
+						if i[j] in'aeiou' or i[j] in "AEIOU":
+							vowel=1
+							print "inside consonant {}".format(i[j])
+							temp = ''.join([i[j:],i[:j],'ay'])
+							LatinWords.append(temp)
+							break
+					if vowel == 0:
+						temp = ''.join([i,'ay'])
+						LatinWords.append(temp)
+
+			#Pig = ('[' + ', '.join(Pigwords) + ']')
+			#Pig = Pig.replace(',', '')
+			#print Pig
+			for words in LatinWords:
+				PigLatin=PigLatin+words+" "
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write(str(PigLatin))
+		return
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+print "Serving at port", PORT
+httpd.serve_forever(),i[:j]
---
+++ test.sh
@@ -0,0 +1,117 @@
+#!/bin/sh 
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=pig" localhost:8000/vicarious)
+if [ "$result" == "igpay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=banana" localhost:8000/vicarious)
+if [ "$result" == "ananabay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=trash" localhost:8000/vicarious)
+if [ "$result" == "ashtray " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=duck" localhost:8000/vicarious)
+if [ "$result" == "uckday " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=glove" localhost:8000/vicarious)
+if [ "$result" == "oveglay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=eat" localhost:8000/vicarious)
+if [ "$result" == "eatyay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=omelet" localhost:8000/vicarious)
+if [ "$result" == "omeletyay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the Standard Inputs'
+result=$(curl -s -XPOST --data "input=are" localhost:8000/vicarious)
+if [ "$result" == "areyay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the  Inputs in uppercase in the middle of the word'
+result=$(curl -s -XPOST --data "input=frUit" localhost:8000/vicarious)
+if [ "$result" == "Uitfray " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing the input in  uppercase for a consonant'
+result=$(curl -s -XPOST --data "input=Fruit" localhost:8000/vicarious)
+if [ "$result" == "uitFray " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+
+echo 'Testing the  input in uppercase for a vowel'
+result=$(curl -s -XPOST --data "input=Apple" localhost:8000/vicarious)
+if [ "$result" == "Appleyay " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing a sentence'
+result=$(curl -s -XPOST --data "input=The quick brown fox jump over the lazy dog" localhost:8000/vicarious)
+if [ "$result" == "eThay uickqay ownbray oxfay umpjay overyay ethay azylay ogday " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing a sentence'
+result=$(curl -s -XPOST --data "input=Hen, pig, fox, dOg" localhost:8000/vicarious)
+if [ "$result" == "en,Hay ig,pay ox,fay Ogday " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+echo 'Testing a sentence'
+result=$(curl -s -XPOST --data "input=Hen. pig. fox. dOg" localhost:8000/vicarious)
+if [ "$result" == "en.Hay ig.pay ox.fay Ogday " ]; then
+	echo "The expected result matched the actual result test passed"
+else
+	echo "The expected result did not match the actual result test failed"
+fi
+
+# echo 'Testing a sentence with Periods'
+# curl -XPOST --data "input=Hen.pig.fox.dOg" localhost:8000/vicarious
